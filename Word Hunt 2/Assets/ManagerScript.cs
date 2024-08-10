3c45e9e3082651c7be9a0dc003b3eb063f49d74a
aria nguyen <arianakamura@gmail.com>
2024-08-10 00:10:22 -0500

    Update ManagerScript.cs

--- Word Hunt 2/Assets/ManagerScript.cs
+++ Word Hunt 2/Assets/ManagerScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class ManagerScript : MonoBehaviour
 {
+    public HashSet<string> validWords;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadWords();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void LoadWords()
+    {
+        TextAsset wordFile = Resources.Load<TextAsset>("words");
+        validWords = new HashSet<string>();
+        string[] words = wordFile.text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in words)
+        {
+            validWords.Add(word);
+        }
+        Debug.Log($"Loaded {validWords.Count} words.");
     }
 }
