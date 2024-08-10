5bc88ddab5df36879c1f26d5ad465d23fb75573c
onoderalance <onoderalance@gmail.com>
2024-08-10 14:31:51 +0900

    added some letter things

--- Word Hunt 2/Assets/TileScript.cs
+++ Word Hunt 2/Assets/TileScript.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class TileScript : MonoBehaviour
 {
     public char letter;
+    public TextMeshProUGUI textMeshPro;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        letter = 'c';
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateLetterText()
+    {
+        // Update the text with the current letter value
+        textMeshPro.text = letter.ToString();
     }
 }
