ddce51e29c1a246066286e3bb676b6a80611dd4d
onoderalance <onoderalance@gmail.com>
2024-08-12 17:02:17 +0900

    basic start menu and button functionality

---
+++ Word Hunt 2/Assets/StartScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D collider = Physics2D.OverlapPoint(mousePosition);
+
+            if (collider != null)
+            {
+                Debug.Log("Start clicked");
+                //start game
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+}
